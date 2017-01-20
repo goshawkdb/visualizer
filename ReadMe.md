632c9e6169f35fe54a77e87dcf9db9b6a004796b
Adam Iley <adam.iley@db.com>
2017-01-20 11:31:43 +0000

    initial checkin

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+GoshawkDB Visualizer
+====================
+
+A graph visualizer for goshawkdb.
+
+For development, you can use <a href="https://rawgit.com/goshawkdb/visualizer/master/index.html">this link</a>.
+
+Parameters
+----------
+
+You can provide the host and wss port of the goshawkdb server with query parameters, e.g.
+ `https://rawgit.com/goshawkdb/visualizer/master/index.html?host=localhost&port=7895`
+otherwise the default `localhost` and `7895` will be used.
---
+++ example-configurations.js
@@ -0,0 +1,22 @@
+function stringToArrayBuffer(string) {
+	return new TextEncoder('utf-8').encode(string).buffer
+}
+
+// This is an unused function that you can execute from the developer console
+// if you want to see the data change.
+function populate() {
+	goshawkdb.connect(`wss://${params.host}:${params.port}/ws`).then((connection) => {
+		connection.transact((txn) => {
+			const root = txn.roots["myRoot"]
+			const o1 = txn.create(stringToArrayBuffer("Hello"), [root])
+			const o2 = txn.create(Uint8Array.from([0, 20, 40, 60, 90, 100, 32, 52, 91, 200, 65, 67, 78, 92, 35, 10, 19, 20, 39, 40, 41, 42, 43, 100, 101, 102, 103, 104]), [])
+			const o3 = txn.create(stringToArrayBuffer("Maybe"), [o2, o1])
+			const o4 = txn.create(stringToArrayBuffer("stuff"), [o3])
+
+			txn.write(root, stringToArrayBuffer("ROOT"), [o4, o1.denyRead()])
+		})
+	}).catch((err) => {
+		console.log("Connection Failed", err)
+	})
+}
+
